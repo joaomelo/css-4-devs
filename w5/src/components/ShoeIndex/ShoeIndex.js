6fa4913734b50fddcd57f6cf5f38b537464aeae6
joão melo <j@melo.plus>
2021-07-22 10:02:42 -0300

    feat: w5-e3 done

--- w5/src/components/ShoeIndex/ShoeIndex.js
+++ w5/src/components/ShoeIndex/ShoeIndex.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { WEIGHTS } from '../../constants';
+import { WEIGHTS, QUERIES } from '../../constants';
 
 import Breadcrumbs from '../Breadcrumbs';
 import Select from '../Select';
@@ -14,33 +14,62 @@ const ShoeIndex = ({ sortId, setSortId }) => {
     <Wrapper>
       <MainColumn>
         <Header>
-          <Title>Running</Title>
-          <Select
-            label="Sort"
-            value={sortId}
-            onChange={(ev) => setSortId(ev.target.value)}
-          >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
-          </Select>
+          <div>
+            <MobileShoeBreadcrumbs><ShoeBreadcrumbs /></MobileShoeBreadcrumbs>
+            <Title>Running</Title>
+          </div>
+          <SortFilterWrapper>
+            <Select
+              label="Sort"
+              value={sortId}
+              onChange={(ev) => setSortId(ev.target.value)}
+            >
+              <option value="newest">Newest Releases</option>
+              <option value="price">Price</option>
+            </Select>
+          </SortFilterWrapper>
         </Header>
         <Spacer size={32} />
         <ShoeGrid />
       </MainColumn>
       <LeftColumn>
-        <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
-        </Breadcrumbs>
+        <DesktopShoeBreadcrumbs><ShoeBreadcrumbs /></DesktopShoeBreadcrumbs>
         <Spacer size={42} />
         <ShoeSidebar />
       </LeftColumn>
     </Wrapper>
   );
 };
+
+
+function ShoeBreadcrumbs(props) {
+  return (
+    <Breadcrumbs {...props}>
+      <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+      <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+      <Breadcrumbs.Crumb href="/sale/shoes">Shoes</Breadcrumbs.Crumb>
+    </Breadcrumbs>  
+  )
+}
+
+const DesktopShoeBreadcrumbs = styled.div`
+  @media(${QUERIES.tabletAndSmaller}) {
+    display: none;
+  }
+`
+
+const MobileShoeBreadcrumbs = styled.div`
+  display: none;
+  @media(${QUERIES.tabletAndSmaller}) {
+    display: revert;
+  }
+`
+
+const SortFilterWrapper = styled.div`
+  @media(${QUERIES.phoneAndSmaller}) {
+    display: none;
+  }
+`
 
 const Wrapper = styled.div`
   display: flex;
@@ -51,6 +80,10 @@ const Wrapper = styled.div`
 
 const LeftColumn = styled.div`
   flex-basis: 248px;
+
+  @media(${QUERIES.tabletAndSmaller}) {
+    display: none;
+  }
 `;
 
 const MainColumn = styled.div`
@@ -61,6 +94,10 @@ const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: baseline;
+
+  @media(${QUERIES.tabletAndSmaller}) {
+    align-items: flex-end;
+  }
 `;
 
 const Title = styled.h2`
